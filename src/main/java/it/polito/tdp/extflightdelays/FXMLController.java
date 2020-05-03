92736e9c5c9669a2e2221da31fe1c38a118f38b7
samuel <samuel@localhost>
2020-05-03 23:06:11 +0200

    progetto completo funzionante

--- src/main/java/it/polito/tdp/extflightdelays/FXMLController.java
+++ src/main/java/it/polito/tdp/extflightdelays/FXMLController.java
@@ -5,6 +5,7 @@
 package it.polito.tdp.extflightdelays;
 
 import java.net.URL;
+
 import java.util.ResourceBundle;
 
 import org.jgrapht.Graph;
@@ -18,6 +19,31 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+
+//COMMENTI LABORATORIO 8 
+
+/*
+ 	Dobbiamo costruire un grafo semplice, non orientato e pesato.
+ 	I vertici sono gli aereoporti e siccome i voli vanno in entrambe le direzioni e noi vogliamo invece un grafo non orientato, la distanza media
+ 	e' da calcolare tenendo conto di entrambe le direzioni (e la teniamo solo se superiamo una certa soglia).
+ 	
+ 	Gli aereoporti li importiamo come lista dal database e poi li convertiamo nel model in map perche' li usiamo come identity map per quando leggiamo
+ 	la tabella dei voli e creiamo le connessioni.
+ 	
+ 	Le distanze sullo stesso percorso nel database sono praticamente sempre le stesse, ma noi dobbiamo considerare che possano variare e quindi per 
+ 	questo si va a calcolare la distanza media.
+ 	
+ 	Importiamo le connessioni unidirezionali (perche' con le query non riesco gia' a mettere tutto insieme) e con una group by ci salviamo la distanza
+ 	totale su questi e il numero di voli per poterci poi creare la media nella maniera corretta.
+ 	
+ 	Aggiungiamo tutti gli aereoporti al grafo come vertici (importante che il DAO ritorni le liste in quanto come veritici del grafo possiamo aggiungere
+ 	liste, ma non mappe) e per le connessioni dobbiamo scorrerle manualmente per controllare entrambe le direzioni.
+ 	
+ 	Supponiamo che la tabella dei voli sia caricata correttamente e quindi che ci siano numeri dove devono esserci con voli e id di aereoporti che esistono
+ 	e che possiamo riscontare nelle rispettive tabelle.
+ 	
+ 	
+ */
 
 public class FXMLController {
 
